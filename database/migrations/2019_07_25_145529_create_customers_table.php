6845b6b984f67e302757ffebbd43237ed5d6a69b
An <havanan95@gmail.com>
2019-07-26 00:03:59 +0700

    add theme file

--- database/migrations/2019_07_25_145529_create_customers_table.php
+++ database/migrations/2019_07_25_145529_create_customers_table.php
@@ -15,6 +15,7 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
