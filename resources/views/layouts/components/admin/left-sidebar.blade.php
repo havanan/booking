bedddd3d6df6f7604820483ade1a3bfb9906204f
anhv <havanan95@gmail.com>
2019-07-26 15:51:01 +0700

    thêm menu left side bar

--- resources/views/layouts/components/admin/left-sidebar.blade.php
+++ resources/views/layouts/components/admin/left-sidebar.blade.php
@@ -10,24 +10,36 @@
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
                 <li> <a class="waves-effect waves-dark" href="{{route('admin.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Dashboard</span></a></li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-layout-grid2"></i><span class="hide-menu">Apps</span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="app-calendar.html">Calendar <i class="ti-calendar"></i></a></li>
-                        <li><a href="app-chat.html">Chat app <i class="ti-comment"></i></a></li>
-                        <li><a href="app-ticket.html">Support Ticket <i class="ti-support"></i></a></li>
-                        <li><a href="app-contact.html">Contact / Employee <i class="ti-user"></i></a></li>
-                        <li><a href="app-contact2.html">Contact Grid <i class=" ti-list"></i></a></li>
-                        <li><a href="app-contact-detail.html">Contact Detail <i class="ti-pencil-alt"></i></a></li>
-                    </ul>
-                </li>
-                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Inbox</span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="app-email.html">Mailbox <i class="icon-envelope-open"></i></a></li>
-                        <li><a href="app-email-detail.html">Mailbox Detail <i class="ti-layout-media-left-alt"></i></a></li>
-                        <li><a href="app-compose.html">Compose Mail <i class="icon-note"></i></a></li>
-                    </ul>
-                </li>
                 <li class="nav-small-cap"></li>
+                    <li> <a class="waves-effect waves-dark" href="{{route('admin.tour_categories.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Loại Tour</span></a></li>
+                    <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Tour</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="app-email.html">Danh sách <i class="icon-envelope-open"></i></a></li>
+                            <li><a href="app-email-detail.html">Bảng giá <i class="ti-layout-media-left-alt"></i></a></li>
+                        </ul>
+                    </li>
+                    <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Booking</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="app-email.html">Danh sách <i class="icon-envelope-open"></i></a></li>
+                        </ul>
+                    </li>
+                    <li> <a class="waves-effect waves-dark" href="{{route('admin.tour_categories.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Dịch vụ đi kèm</span></a></li>
+                <li class="nav-small-cap"></li>
+                    <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Nhân viên</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="app-email.html">Danh sách <i class="icon-envelope-open"></i></a></li>
+                        </ul>
+                    </li>
+                    <li> <a class="waves-effect waves-dark" href="{{route('admin.tour_categories.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Khách hàng</span></a></li>
+                <li class="nav-small-cap"></li>
+                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-email"></i><span class="hide-menu">Bài viết</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="app-email.html">Danh sách <i class="icon-envelope-open"></i></a></li>
+                    </ul>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="{{route('admin.tour_categories.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Loại bài viết</span></a></li>
+                <li class="nav-small-cap"></li>
+                    <li> <a class="waves-effect waves-dark" href="{{route('admin.report.index')}}" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Báo cáo</span></a></li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
